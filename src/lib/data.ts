db0574bc08d9f6549421ddb11647807515f72e4a
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-08-11 11:50:41 +0530

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,16 +1,3 @@
 export const Tfn1: string = "+91 88106 04605";
 export const Tfn2: string = "+91 83682 80443";
 export const email: string = "abhi982114@gmail.com";
-
-
-// MONGODB_URI=mongodb+srv://suryamani_kumar_98:kumar_1998_surymani@backpackpluse.pstcr3c.mongodb.net/?appName=backpackpluse
-// NEXT_PUBLIC_API_BASE_URL=/api
-// SMTP_HOST=smtp.gmail.com
-// SMTP_PORT=587
-// SMTP_USER=8437suryamanikumar@gmail.com
-// SMTP_PASSWORD=jxyb nlun unim kyam
-// SMTP_FROM="Your Store <your-email@gmail.com>"
-// JWT_SECRET=super-secret-gestcms-jwt-key-change-in-production
-// CLOUDINARY_CLOUD_NAME=djrtuyxoj
-// CLOUDINARY_API_KEY=346643697443813
-// CLOUDINARY_API_SECRET=hYUJDGmiIjyv_Haqg3ReuU7zk0k
