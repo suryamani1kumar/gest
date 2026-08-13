567a9e5ba3d85407ecee191391964b6a0fe574fb
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-08-13 09:51:56 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,14 @@
 import { useState } from "react";
 import BottomNav from "./BottomNav";
 import Navbar from "./TopNavbar";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export default function Header() {
   const [accountOpen, setAccountOpen] = useState(false);
+  const { customer, isAuthenticated, loading } = useSelector(
+    (state: RootState) => state.auth,
+  );
   return (
     <header className="fixed top-0 left-0 w-full bg-white z-50 shadow-sm">
       {/* Top Header */}
