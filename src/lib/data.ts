9270d16b1da4b98e29c9db49f9c3bf4c2d6fc6dd
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-08-31 17:40:44 +0530

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,3 @@
-export const Tfn1: string = "+91 88106 04605";
+export const Tfn1: string = "+91 92133 32134";
 export const Tfn2: string = "+91 83682 80443";
 export const email: string = "abhi982114@gmail.com";
